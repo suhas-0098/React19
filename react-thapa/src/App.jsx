893fd3b90239361d3b0bb61ab890d01400adb7ca
suhas-0098 <190030098cse@gmail.com>
2026-07-06 21:50:47 +0530

    dynamic values in jsx

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -10,14 +10,22 @@ export const App = () =>{
 }
 
 const NetflixSeries = ()=>{
+  const name ="Game of Thrones";
+  const rating = "8.5";
+  const summary ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum sed autem. Accusantium debitis nemo sequi dolore consequuntur veniam. Sed.";
+  const returnGenre = ()=>{
+    const genre = "RomCom";
+    return genre;
+  }
   return(
      <div>
         <div>
           <img src="https://m.media-amazon.com/images/M/MV5BMTNhMDJmNmYtNDQ5OS00ODdlLWE0ZDAtZTgyYTIwNDY3OTU3XkEyXkFqcGc@._V1_.jpg" alt=""  width= "40%" height="40%"/>
         </div>
-      <h2>Name : Game of Thrones </h2>
-      <h3>Rating:8.2</h3>
-      <p>Summary : Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum sed autem. Accusantium debitis nemo sequi dolore consequuntur veniam. Sed.</p>
+      <h2>Name : {name}</h2>
+      <h3>Rating: {rating}</h3>
+      <p>Summary : {summary}</p>
+      <p>Genre : {returnGenre()}</p>
     </div>
   );
 }
