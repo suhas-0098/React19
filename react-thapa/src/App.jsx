73017f64362ff35871cdb7f07bd96877c9d04341
suhas-0098 <190030098cse@gmail.com>
2026-07-06 18:31:32 +0530

    first commit

---
+++ react-thapa/src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react"
+
+export const App = () =>{
+  return <div>
+
+    <div>
+      <img src="https://m.media-amazon.com/images/M/MV5BMTNhMDJmNmYtNDQ5OS00ODdlLWE0ZDAtZTgyYTIwNDY3OTU3XkEyXkFqcGc@._V1_.jpg" alt=""  width= "40%" height="40%"/>
+      <h2>Name : Game of Thrones </h2>
+      <h3>Rating:8.2</h3>
+      <p>Summary : Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum sed autem. Accusantium debitis nemo sequi dolore consequuntur veniam. Sed.</p>
+    </div>
+  </div>
+}
+
+
+
