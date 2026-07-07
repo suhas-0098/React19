8304dbecae56ae7c1dffa7373742200e341e7fa9
suhas-0098 <190030098cse@gmail.com>
2026-07-07 22:43:29 +0530

    conditional css

--- react-thapa/src/components/Lists.jsx
+++ react-thapa/src/components/Lists.jsx
@@ -4,10 +4,12 @@ export const SeriesCard = ({key,series}) =>{
     padding: "1.2rem 2.4rem",
     border:"none",
     fontSize:"1.6rem",
-    backgroundColor:"var(--btn-hover-bg-color)",
-    color:"var(--bg-color)"
+    backgroundColor:`${series.rating>=8.5 ? "#7DCEA0":"#f7dc6f"}`,
+    color:"var(--bg-color)",
+    fontWeight:"bold",
+    cursor:"pointer"
   }
-
+  const ratingClass = series.rating>=8.5 ? "super_hit":"average";
   return(
     <li className="card">
           <div>
@@ -16,7 +18,7 @@ export const SeriesCard = ({key,series}) =>{
         </div>
          <div className="card-content">
           <h2>Name : {series.name}</h2>
-      <h3>Rating: {series.rating}</h3>
+      <h3>Rating: <span className={`rating ${ratingClass}`}>{series.rating}</span> </h3>
       <p >Summary : {series.description}</p>
       <p>Genre : {series.genre}</p>
       <p>Cast : {series.cast}</p>
