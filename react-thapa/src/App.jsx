ea1e71dd0c17312ca3409538d30d11cd9bae9e67
suhas-0098 <190030098cse@gmail.com>
2026-07-08 16:56:50 +0530

    event handling

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -7,6 +7,7 @@ import { NetflixSeries } from "./components/NetflixSeries"
 
 // import "./components/Netflix.css";
 import './components/Netflix.module.css';
+import { EventHandling } from "./components/EventHandling";
 export const App = () =>{
   return(
 
@@ -14,7 +15,7 @@ export const App = () =>{
 
  <h1 className="card-heading">List of Best Netflix Series</h1>
     <NetflixSeries/>
-   
+    <EventHandling/>
     
   </section>
   )
