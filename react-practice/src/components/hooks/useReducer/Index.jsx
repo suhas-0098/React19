27ba3a6bbddde22f3d638f093ddacd6faf179269
suhas-0098 <190030098cse@gmail.com>
2026-07-10 17:05:23 +0530

    best practice for useReducer hook

--- react-practice/src/components/hooks/useReducer/Index.jsx
+++ react-practice/src/components/hooks/useReducer/Index.jsx
@@ -1,27 +1,50 @@
-import { useReducer } from "react"
+import { useReducer } from "react";
 
-export const Index = () =>{
+export const Index = () => {
+  const initialState = {
+    count: 0,
+    inc: 2,
+    dec: 2,
+  };
 
-  const reducer = (state,action) =>{
+  const reducer = (state, action) => {
+    console.log(state, action);
+    // if (action.type === "INCREMENT") {
+    //   return state + 1;
+    // }
+    // if (action.type === "DECREMENT") {
+    //   return state - 1;
+    // }
+    // if (action.type === "RESET") {
+    //   return (state = 0);
+    // }
 
-    if(action.type==="INC"){
-     return state +1;
+    switch (action.type) {
+      case "INCREMENT":
+        return {
+          ...state,
+          count: state.count + 1,
+        };
 
+      case "DECREMENT":
+        return { ...state, count: state.count - 1 };
+
+      case "RESET":
+        return { ...state, count: 0 };
+
+      default:
+        return state;
     }
-    if(action.type==="DEC"){
-      return state-1;
-    }
+  };
 
-  }
-
-  const [count,dispatch] = useReducer(reducer,0);
-
-  return <>
-    <div  className=" p-4 h-lvh flex flex-col justify-center items-center ">
-      <h1>Hello from useReducer</h1>
-   <h1>Count : {count}</h1>
-   <button onClick={()=>dispatch({type:"INC"})}>Increment</button>
-   <button onClick={()=>dispatch({type:"DEC"})}>Decrement</button>
+  //   const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className=" p-4 h-lvh flex flex-col justify-center items-center ">
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
     </div>
-  </>
-}
+  );
+};
